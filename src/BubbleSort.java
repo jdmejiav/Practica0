1cfa97238c65aa4d4b3af61d00d7e927b78feceb
jdmejiav <jdmejiav@eafit.edu.co>
2020-02-11 18:57:34 -0500

    tercer commit

--- src/BubbleSort.java
+++ src/BubbleSort.java
@@ -2,11 +2,31 @@ import java.util.Scanner;
 
 public class BubbleSort{
 
-	public BubbleSort(){
-		
-	}
-	
+	private int arr[];
 
+	public BubbleSort(int []arr){
+		this.arr=arr;
+		ordenar();
+		System.out.println("Antes de imprimir");
+		imprimir();
+		System.out.println("Después de imprimir");
+	}
+	public void ordenar(){
+		for (int i = 0;i<this.arr.length-1;i++){
+			for (int j = 0;j <this. arr.length-i;i++){
+				if (arr[j]>arr[j+1]){
+					int temp = this.arr[j];
+					this.arr[j]= this.arr[j+1];
+					this.arr[j+1]=temp;
+				}
+			}
+		}
+	}
+	public void imprimir(){
+		for (int i =0;i<this.arr.length;i++){
+			System.out.println(this.arr[i]);
+		}
+	}
 
 
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-
+//import BubbleSort;
 public class Main{
 
 	private int max;
@@ -8,30 +8,24 @@ public class Main{
 	public static void main(String []args){
 		Main main = new Main();
 		main.max=0;
-		main.min=0;		
+		main.min=0;
 		Scanner sc = new Scanner (System.in);
 		int size = Integer.parseInt(sc.nextLine().trim());
 		int arr[] = main.generarNumeros(size);
 		System.out.println("Let's will be relistic, it wouldn't ever finished");
+		BubbleSort bb = new BubbleSort(arr);
 	}
 
 	private int[] generarNumeros(int size){
 		int arr[]= new int[size];
-		
+
 		for (int i=0;i<size;i++){
 			arr[i]=(int)(Math.random()*10001);
 			if (this.max<arr[i])this.max=arr[i];
 			if (this.min>arr[i])this.min=arr[i];
 		}
 		return arr;
-		
-	}
-	private void printNumbers(int []arr){
-		for (int i=0;i<arr.length;i++){
-			System.out.println(arr[i]);
-		}
-
-
 	}
 
 }
+
