65b744ae59ef906562b4c2c9f5d9533f6ab45701
jdmejiav <jdmejiav@eafit.edu.co>
2020-02-28 17:32:21 -0500

    menu

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,35 @@
 import java.util.Scanner;
 
 public class Main{
-
-	private int max;
 	private int min;
+	private int max;
+	public Main(){
+		inicializar();
+	}
 
 	public static void main(String []args){
 		Main main = new Main();
-		main.max=0;
-		main.min=0;
-		Scanner sc = new Scanner (System.in);
-		int size = Integer.parseInt(sc.nextLine().trim());
-		int arr[] = main.generarNumeros(size);
-		BubbleSort bb = new BubbleSort(arr.clone());
-		HybridSort gb = new HybridSort(arr);
 	}
+
+	private void inicializar(){
+		Scanner sc = new Scanner (System.in);
+		System.out.println("Ingrese ");
+		int size = Integer.parseInt(sc.nextLine().trim());
+		int arr[] = generarNumeros(size);
+		String opcion="";
+		do {
+			System.out.println("1). Algoritmo de burbuja");
+			System.out.println("2). Algoritmo hibrido");
+			System.out.println("3). Salir");
+			opcion = sc.nextLine();
+			if (opcion.trim().equals("1")){
+				BubbleSort bb = new BubbleSort(arr.clone());
+			}else if (opcion.trim().equals("2")){
+				HybridSort hb = new HybridSort(arr.clone());
+			}
+		}while(opcion.trim().equals("3"));
+	}
+
 
 	private int[] generarNumeros(int size){
 		long inicio=System.currentTimeMillis();
@@ -29,6 +44,7 @@ public class Main{
 		System.out.println("Tiempo algoritmo generador de números "+(fin-inicio)+"(ms)");
 		return arr;
 	}
+
 	private void printNumbers(int arr[]){
 		for (int i =0 ;i<arr.length;i++){
 			System.out.println(arr[i]);
